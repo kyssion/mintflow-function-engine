c309c148c6bcaaf9597fbefd79213597eb33e80a
kyssion <1409915687@qq.com>
2019-06-11 14:11:04 +0800

    update

--- src/main/java/com/kyssion/galaxy/Galaxy.java
+++ src/main/java/com/kyssion/galaxy/Galaxy.java
@@ -3,16 +3,23 @@ package com.kyssion.galaxy;
 import com.kyssion.galaxy.handle.Handle;
 import com.kyssion.galaxy.process.Process;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Galaxy {
+
     private Map<String, Handle> handleMap;
     private Map<String, Class<? extends Process>> processMap;
+    private Map<Class<? extends Process>, Object> proxy;
 
     public Galaxy(Map<String, Handle> handleMap,
                   Map<String, Class<? extends Process>> processMap) {
         this.handleMap = handleMap;
         this.processMap = processMap;
+        proxy = new HashMap<>();
+        initProxy();
     }
+    private void initProxy() {
 
+    }
 }
