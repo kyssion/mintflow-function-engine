7f8d8f653544a70a2240e9565a1dc31427e7d15c
kyssion <1409915687@qq.com>
2019-06-19 23:04:51 +0800

    update

---
+++ src/main/java/com/kyssion/galaxy/script/builder/NodeBuilder.java
@@ -0,0 +1,5 @@
+package com.kyssion.galaxy.script.builder;
+
+public class NodeBuilder {
+
+}
--- src/main/java/com/kyssion/galaxy/script/translater/analysis/GrammaAnalysis.java
+++ src/main/java/com/kyssion/galaxy/script/translater/analysis/GrammaAnalysis.java
@@ -1,7 +1,15 @@
 package com.kyssion.galaxy.script.translater.analysis;
 
 /**
+ * a = namespaceId
+ * b = processid
+ * c = handleid
+ * Z = SZ|#
+ * S = namespace(a){K}
+ * K = process(b)P;K|#
+ * P = ->h:{c}P|#
  * 语法分析树
  */
 public class GrammaAnalysis {
+
 }
