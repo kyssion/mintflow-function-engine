014af3ad349e687ec02b9f42457d2558dd1a3012
kyssion <1409915687@qq.com>
2019-06-26 19:23:35 +0800

    update

---
+++ src/main/java/com/kyssion/galaxy/scheduler/HandlerScheduler.java
@@ -0,0 +1,16 @@
+package com.kyssion.galaxy.scheduler;
+
+import com.kyssion.galaxy.handle.Handle;
+import com.kyssion.galaxy.param.ParamWrapper;
+
+import java.util.List;
+
+public class HandlerScheduler implements Scheduler {
+    @Override
+    public ParamWrapper run(ParamWrapper paramWrapper, List<Handle> handleList) {
+        for (Handle handle:handleList){
+            paramWrapper = handle.handle(paramWrapper);
+        }
+        return paramWrapper;
+    }
+}
--- src/main/java/com/kyssion/galaxy/scheduler/Scheduler.java
+++ src/main/java/com/kyssion/galaxy/scheduler/Scheduler.java
@@ -1,8 +1,13 @@
 package com.kyssion.galaxy.scheduler;
 
+import com.kyssion.galaxy.handle.Handle;
+import com.kyssion.galaxy.param.ParamWrapper;
+
+import java.util.List;
+
 /**
  *
  */
 public interface Scheduler {
-
+    ParamWrapper run(ParamWrapper paramWrapper, List<Handle> handleList);
 }
