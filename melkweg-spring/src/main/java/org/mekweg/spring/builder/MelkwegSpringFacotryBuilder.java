d59b2743c93f39863056598b7b9b9cfe9887ac69
kyssion <1409915687@qq.com>
2019-08-04 22:15:14 +0800

    update

--- melkweg-spring/src/main/java/org/mekweg/spring/builder/MelkwegSpringFacotryBuilder.java
+++ melkweg-spring/src/main/java/org/mekweg/spring/builder/MelkwegSpringFacotryBuilder.java
@@ -1,32 +1,52 @@
 package org.mekweg.spring.builder;
 
 import org.mekweg.builder.AbstractMelkwegFactoryBuilder;
+import org.mekweg.factory.DefaultMelkwegFactory;
 import org.mekweg.factory.MelkwegFactory;
 
-import java.io.InputStream;
-import java.io.Reader;
+import java.io.*;
 import java.util.Properties;
 
 public class MelkwegSpringFacotryBuilder implements AbstractMelkwegFactoryBuilder {
 
     @Override
     public MelkwegFactory build(Reader reader) {
-        return null;
+        Properties properties = new Properties();
+        try {
+            properties.load(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return build(properties);
     }
 
     @Override
     public MelkwegFactory build(Properties properties) {
-        return null;
+        MelkwegFactory melkwegFactory = new DefaultMelkwegFactory(
+                getPropString(properties, "galaxy.handle-path"),
+                getPropString(properties, "galaxy.map-path"),
+                getPropString(properties, "galaxy.process-path"));
+        return melkwegFactory;
     }
 
     @Override
     public MelkwegFactory build(InputStream inputStream) {
-        return null;
+        return build(new InputStreamReader(inputStream));
+    }
+
+    private String getPropString(Properties properties, String key) {
+        return (String) properties.get(key);
     }
 
     @Override
     public MelkwegFactory build(String configPath) {
-        return null;
+        File file = new File(configPath);
+        try {
+            return build(new FileInputStream(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
 
--- melkweg-spring/src/main/java/org/mekweg/spring/factory/MelkwegSpringFactory.java
+++ melkweg-spring/src/main/java/org/mekweg/spring/factory/MelkwegSpringFactory.java
@@ -1,4 +1,20 @@
 package org.mekweg.spring.factory;
 
-public class MelkwegSpringFactory {
+import org.mekweg.Melkweg;
+import org.mekweg.factory.MelkwegFactory;
+
+public class MelkwegSpringFactory implements MelkwegFactory {
+    private String[] handlePath;
+    private String[] processPath;
+    private String[] mapperPath;
+    public MelkwegSpringFactory(String handPath, String mapperPath, String processPath) {
+        this.handlePath = (handPath == null) ? null : handPath.split(",");
+        this.mapperPath = (mapperPath == null) ? null : mapperPath.split(",");
+        this.processPath = (processPath == null) ? null : processPath.split(",");
+    }
+
+    @Override
+    public Melkweg create() {
+        return null;
+    }
 }
