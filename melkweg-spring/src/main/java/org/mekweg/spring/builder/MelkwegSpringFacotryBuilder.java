3ebc532afc54fab643ae689975c6bf04d6c3eed2
kyssion <1409915687@qq.com>
2019-08-05 11:52:22 +0800

    update

--- melkweg-spring/src/main/java/org/mekweg/spring/builder/MelkwegSpringFacotryBuilder.java
+++ melkweg-spring/src/main/java/org/mekweg/spring/builder/MelkwegSpringFacotryBuilder.java
@@ -6,11 +6,9 @@ import org.mekweg.factory.MelkwegFactory;
 import java.util.Properties;
 
 public class MelkwegSpringFacotryBuilder implements AbstractMelkwegFactoryBuilder {
-
     @Override
     public MelkwegFactory build0(Properties properties) {
         return null;
     }
-
 }
 
