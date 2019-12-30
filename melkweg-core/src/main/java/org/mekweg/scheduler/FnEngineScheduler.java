b2906c5270619061d804b4946c14f8443ed9c214
kyssion <1409915687@qq.com>
2019-12-30 16:19:20 +0800

    feat: create FnEngineScheduler

---
+++ melkweg-core/src/main/java/org/mekweg/scheduler/FnEngineScheduler.java
@@ -0,0 +1,13 @@
+package org.mekweg.scheduler;
+
+import org.mekweg.handle.Handler;
+import org.mekweg.param.ParamWrapper;
+
+import java.util.List;
+
+public class FnEngineScheduler implements Scheduler {
+    @Override
+    public ParamWrapper run(ParamWrapper paramWrapper, List<Handler> handlerList) {
+        return null;
+    }
+}
