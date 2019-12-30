3e25cf34446159d3899804d495a4751fb9eb6b69
kyssion <1409915687@qq.com>
2019-12-30 19:38:28 +0800

    update

--- melkweg-core/src/main/java/org/mekweg/handle/Handler.java
+++ melkweg-core/src/main/java/org/mekweg/handle/Handler.java
@@ -4,8 +4,27 @@ import org.mekweg.param.ParamWrapper;
 import org.mekweg.scheduler.Scheduler;
 
 public abstract class Handler implements Cloneable{
+    private String name;
+    private int code;
     private HandleType type;
     private Scheduler scheduler;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Scheduler getScheduler() {
         return scheduler;
     }
