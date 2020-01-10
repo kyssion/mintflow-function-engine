82b8d2982848454f188fa3a25f26ae8cbce5f9f1
kyssion <liyuelin@xiaomi.com>
2020-01-10 17:24:14 +0800

    feat: add test index

--- melkweg-core/src/test/java/org/mekweg/test/TestForkJoin.java
+++ melkweg-core/src/test/java/org/mekweg/test/TestForkJoin.java
@@ -1,49 +1,81 @@
 package org.mekweg.test;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
 import java.util.concurrent.RecursiveAction;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.RecursiveTask;
 
 public class TestForkJoin {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         ForkJoinPool forkJoinPool = new ForkJoinPool(8);
-        MyRecursiveAction myRecursiveAction = new MyRecursiveAction(new int[]{
-                1,2,3,4,5,6,7,8,9
-        },0,8);
-        //阻塞当前线程直到 ForkJoinPool 中所有的任务都执行结束
-        forkJoinPool.awaitTermination(2, TimeUnit.SECONDS);
-
-        // 关闭线程池
-        forkJoinPool.shutdown();
+        MyRecursiveTask myRecursiveAction = new MyRecursiveTask(new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9
+        }, 0, 8);
+        Future<Integer> item = forkJoinPool.submit(myRecursiveAction);
+        System.out.println(item.get());
     }
 }
 
-class MyRecursiveAction extends RecursiveAction{
+class MyRecursiveTask extends RecursiveTask<Integer> {
+
     private int[] itemList;
     private int start;
     private int end;
 
-    public MyRecursiveAction(int[] itemList,int start,int end){
+    public MyRecursiveTask(int[] itemList, int start, int end) {
         this.itemList = itemList;
         this.start = start;
         this.end = end;
     }
+
+    @Override
+    protected Integer compute() {
+        if(start==end){
+            return itemList[start];
+        }
+        MyRecursiveTask left = new MyRecursiveTask(itemList,start,(start+end)/2);
+        MyRecursiveTask right = new MyRecursiveTask(itemList,(start+end)/2+1,end);
+        invokeAll(left,right);
+        return left.join()+right.join();
+    }
+}
+
+class MyRecursiveAction extends RecursiveAction {
+    private int[] itemList;
+    private int start;
+    private int end;
+
+    public MyRecursiveAction(int[] itemList, int start, int end) {
+        this.itemList = itemList;
+        this.start = start;
+        this.end = end;
+    }
+
     /**
      * The main computation performed by this task.
      */
     @Override
     protected void compute() {
-        if(start>end){
+        if (start > end) {
             return;
         }
-        if(end+1-start>3){
-            MyRecursiveAction recursiveAction = new MyRecursiveAction(itemList,start,start+2);
-            MyRecursiveAction recursiveAction2 = new MyRecursiveAction(itemList,start+3,end);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (end + 1 - start > 3) {
+            MyRecursiveAction recursiveAction = new MyRecursiveAction(itemList, start, start + 2);
+            MyRecursiveAction recursiveAction2 = new MyRecursiveAction(itemList, start + 3, end);
             recursiveAction.fork();
+            System.out.println("one end");
             recursiveAction2.fork();
-        }else{
-            while(start<=end){
-                System.out.print(itemList[start]+" ");
+            System.out.println("two end");
+        } else {
+            while (start <= end) {
+                System.out.print(itemList[start] + " ");
+                start++;
             }
             System.out.println();
         }
