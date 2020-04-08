d32c0d449d02b0d95f3222f489cdfc716ef49fe8
kyssion <1409915687@qq.com>
2020-04-08 09:37:01 +0800

    feat: add spring supper

---
+++ melkweg-spring/src/main/java/org/melkweg/spring/handleer/SpringMelkwegHandleDataMapFinder.java
@@ -0,0 +1,7 @@
+package org.melkweg.spring.handleer;
+
+import org.melkweg.handle.util.MelkwegHandleDataMapFinder;
+
+public class SpringMelkwegHandleDataMapFinder extends MelkwegHandleDataMapFinder {
+
+}
