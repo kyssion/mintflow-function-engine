42ed870f13608d6edbcabe2c9caa26a4bf7466d8
kyssion <1409915687@qq.com>
2020-06-21 17:40:07 +0800

    feat: add async temple support

--- melkweg-core/src/main/java/org/melkweg/template/proxy/MelkwegTemplateFunctionProxy.java
+++ melkweg-core/src/main/java/org/melkweg/template/proxy/MelkwegTemplateFunctionProxy.java
@@ -2,6 +2,8 @@ package org.melkweg.template.proxy;
 
 import org.melkweg.Melkweg;
 import org.melkweg.annotation.*;
+import org.melkweg.async.result.AsyncResult;
+import org.melkweg.exception.HandleUseException;
 import org.melkweg.param.ParamWrapper;
 import org.melkweg.scheduler.sync.SyncFnEngineSyncScheduler;
 
@@ -25,7 +27,7 @@ public class MelkwegTemplateFunctionProxy<T> implements InvocationHandler {
     }
 
     @Override
-    public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+    public Object invoke(Object o, Method method, Object[] objects){
         AsyncSupport asyncSupport = method.getAnnotation(AsyncSupport.class);
         if(asyncSupport!=null){
             return runAsync(method,objects);
@@ -36,13 +38,37 @@ public class MelkwegTemplateFunctionProxy<T> implements InvocationHandler {
 
     private Object runAsync(Method method, Object[] objects) {
         String processName = getProcessName(method);
-        ParamWrapper paramWrapper = new ParamWrapper();
-        Annotation[][] annotations = method.getParameterAnnotations();
+        ParamWrapper paramWrapper = getParamWrapper(method,objects);
+        int lastOne = objects.length-1;
+        if(objects.length==0||!(objects[lastOne] instanceof AsyncResult)){
+            throw new HandleUseException("当前未发现，结果回调获取逻辑，method："+method.getName());
+        }
+        AsyncResult asyncResult = (AsyncResult) objects[lastOne];
+        this.melkweg.runAsync(nameSpace,processName,paramWrapper,asyncResult);
         return null;
     }
 
     private Object runSync(Method method, Object[] objects) {
         String processName = getProcessName(method);
+        ParamWrapper paramWrapper = getParamWrapper(method,objects);
+        paramWrapper =  this.melkweg.runSync(nameSpace,processName,paramWrapper,new SyncFnEngineSyncScheduler());
+        if(method.getReturnType()== ParamWrapper.class){
+            return paramWrapper;
+        }
+        return paramWrapper.getResult(method.getReturnType());
+    }
+
+
+    private String getProcessName(Method method){
+        MelkwegProcess melkwegProcess = method.getAnnotation(MelkwegProcess.class);
+        if(melkwegProcess !=null&&!"".equals(melkwegProcess.name())){
+            return  melkwegProcess.name();
+        }else{
+            return method.getName();
+        }
+    }
+
+    private ParamWrapper getParamWrapper(Method method,Object[] objects){
         ParamWrapper paramWrapper = new ParamWrapper();
         Annotation[][] annotations = method.getParameterAnnotations();
         for (int a=0;a<objects.length;a++){
@@ -59,21 +85,7 @@ public class MelkwegTemplateFunctionProxy<T> implements InvocationHandler {
                 }
             }
         }
-        paramWrapper =  this.melkweg.runSync(nameSpace,processName,paramWrapper,new SyncFnEngineSyncScheduler());
-        if(method.getReturnType()== ParamWrapper.class){
-            return paramWrapper;
-        }
-        return paramWrapper.getResult(method.getReturnType());
-    }
-
-
-    private String getProcessName(Method method){
-        MelkwegProcess melkwegProcess = method.getAnnotation(MelkwegProcess.class);
-        if(melkwegProcess !=null&&!"".equals(melkwegProcess.name())){
-            return  melkwegProcess.name();
-        }else{
-            return method.getName();
-        }
+        return paramWrapper;
     }
 
 }
--- melkweg-core/src/test/java/org/melkweg/templateFunction/Function1.java
+++ melkweg-core/src/test/java/org/melkweg/templateFunction/Function1.java
@@ -1,11 +1,8 @@
 package org.melkweg.templateFunction;
 
-import org.melkweg.annotation.MelkwegContextParam;
-import org.melkweg.annotation.MelkwegNameSpace;
-import org.melkweg.annotation.MelkwegParam;
+import org.melkweg.annotation.*;
 import org.melkweg.async.result.AsyncResult;
 import org.melkweg.param.ParamWrapper;
-import org.melkweg.annotation.MelkwegProcess;
 import org.melkweg.template.MelkwegTemplateFunction;
 
 @MelkwegNameSpace(name = "test_namespace")
@@ -18,7 +15,8 @@ public interface Function1 extends MelkwegTemplateFunction {
                       @MelkwegContextParam(key = "condition_4") String condition4,
                       @MelkwegContextParam(key = "show_start") boolean showStart,
                       @MelkwegContextParam(key = "show_end") boolean showEnd);
-
+    @MelkwegProcess(name="async_test_process")
+    @AsyncSupport
     void test(@MelkwegParam Integer num, @MelkwegParam String item,
               @MelkwegContextParam(key = "condition_1") String condition1,
               @MelkwegContextParam(key = "condition_2") String condition2,
