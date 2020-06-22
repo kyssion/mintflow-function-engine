2c2206caa5c1ff060c138f40dd5ab8367079a013
kyssion <1409915687@qq.com>
2020-06-22 17:00:49 +0800

    fix: add vertx case

--- mintflow-vertx/src/test/java/org/mintflow/vertx/RunTest.java
+++ mintflow-vertx/src/test/java/org/mintflow/vertx/RunTest.java
@@ -1,8 +1,14 @@
 package org.mintflow.vertx;
 
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+
 public class RunTest {
     public void VertxRunTest(){
-
-
+        Vertx vertx = Vertx.vertx();
+        HttpServer server = vertx.createHttpServer();
+        server.connectionHandler(httpConnection->{
+            System.out.println(httpConnection.isSsl());
+        });
     }
 }
