4e87ed39fb3318030c2d184bd2f04654ce18012b
kyssion <1409915687@qq.com>
2020-07-05 16:10:25 +0800

    feat: add annotation support

--- mintflow-vertx/src/main/java/org/mintflow/vertx/http/controller/VertxController.java
+++ mintflow-vertx/src/main/java/org/mintflow/vertx/http/controller/VertxController.java
@@ -1,5 +1,11 @@
 package org.mintflow.vertx.http.controller;
 
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Inherited
 public @interface VertxController {
     String url() default "";
     String nameSpace();
--- mintflow-vertx/src/main/java/org/mintflow/vertx/http/controller/VertxRequestMapper.java
+++ mintflow-vertx/src/main/java/org/mintflow/vertx/http/controller/VertxRequestMapper.java
@@ -2,6 +2,12 @@ package org.mintflow.vertx.http.controller;
 
 import io.vertx.core.http.HttpMethod;
 
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Inherited
 public @interface VertxRequestMapper {
     String url() default "";
     HttpMethod[] httpMethod() default {HttpMethod.POST,HttpMethod.GET};
