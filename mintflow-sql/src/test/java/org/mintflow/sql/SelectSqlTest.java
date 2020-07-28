ee3abb9a041bf8f637126676aa586c5f7ef9b557
kyssion <1409915687@qq.com>
2020-07-28 11:03:18 +0800

    feat: add sql test

--- mintflow-sql/src/test/java/org/mintflow/sql/SelectSqlTest.java
+++ mintflow-sql/src/test/java/org/mintflow/sql/SelectSqlTest.java
@@ -8,22 +8,22 @@ import java.util.List;
 
 public class SelectSqlTest {
 
-    public Object[] createArrays(List<Object> list){
+    public Object[] createArrays(List<Object> list) {
         Object[] objects = new Object[list.size()];
-        for(int a=0;a<objects.length;a++){
+        for (int a = 0; a < objects.length; a++) {
             objects[a] = list.get(a);
         }
         return objects;
     }
 
     @Test
-    public void SampleSelectTest(){
-        Sql sql= Select.sql().selectFrom("test_table","name","age").build();
-        Assert.assertEquals(sql.getSql(),"select `name`,`age` from `test_table`");
+    public void SampleSelectTest() {
+        Sql sql = Select.sql().selectFrom("test_table", "name", "age").build();
+        Assert.assertEquals(sql.getSql(), "select `name`,`age` from `test_table`");
     }
 
     @Test
-    public void conditionSelectTest(){
+    public void conditionSelectTest() {
         List<Object> params = new ArrayList<>();
         params.add("jack");
         params.add(1);
@@ -32,34 +32,34 @@ public class SelectSqlTest {
         params.add(4);
         params.add(5);
         params.add(6);
-        Sql sql= Select.sql().selectFrom("test_table","name","age","num","bookNum")
-                .eq("name",params.get(0)).and()
-                .between("age",params.get(1),params.get(2)).and()
-                .gt("num",params.get(3)).and()
-                .lt("num",params.get(4)).and()
-                .gte("bookNum",params.get(5)).and()
-                .ltq("bookNum",params.get(6))
+        Sql sql = Select.sql().selectFrom("test_table", "name", "age", "num", "bookNum")
+                .eq("name", params.get(0)).and()
+                .between("age", params.get(1), params.get(2)).and()
+                .gt("num", params.get(3)).and()
+                .lt("num", params.get(4)).and()
+                .gte("bookNum", params.get(5)).and()
+                .ltq("bookNum", params.get(6))
                 .build();
-        Assert.assertEquals(sql.getSql(),"select `name`,`age`,`num`,`bookNum` from `test_table` where `name` = ? AND (`age` between ? AND ? ) AND `num` > ? AND `num` < ? AND `bookNum` >= ? AND `bookNum` <= ? ");
-        Assert.assertArrayEquals(createArrays(params),createArrays(sql.getParamsList()));
+        Assert.assertEquals(sql.getSql(), "select `name`,`age`,`num`,`bookNum` from `test_table` where `name` = ? AND (`age` between ? AND ? ) AND `num` > ? AND `num` < ? AND `bookNum` >= ? AND `bookNum` <= ? ");
+        Assert.assertArrayEquals(createArrays(params), createArrays(sql.getParamsList()));
     }
 
     @Test
-    public void conditionSelectGroupBy(){
-          List<Object> params = new ArrayList<>();
-          params.add("jack");
-          params.add("bookNum");
-          params.add("name");
-          params.add("age");
-          params.add(1);
-          params.add(2);
-          Select select = Select.sql();
-          select.selectFrom("test_table","name","age","num","bookNum");
-          select.eq("name",params.get(0));
-          select.groupBy((String) params.get(1));
-          select.orderByAsc((String) params.get(2));
-          select.orderByDesc((String) params.get(3));
-          select.limit((int)params.get(4),(int)params.get(5));
-        System.out.println(select.build().getSql());
+    public void conditionSelectGroupBy() {
+        List<Object> params = new ArrayList<>();
+        params.add("jack");
+        params.add(1);
+        params.add(2);
+        Select select = Select.sql();
+        select.selectFrom("test_table", "name", "age", "num", "bookNum");
+        select.eq("name", params.get(0));
+        select.groupBy("name");
+        select.orderByAsc("age");
+        select.orderByDesc("num");
+        select.limit((int) params.get(1), (int) params.get(2));
+
+        Sql sql = select.build();
+        Assert.assertEquals(sql.getSql(),"select `name`,`age`,`num`,`bookNum` from `test_table` where `name` = ? GROUP_BY `bookNum` ORDER BY `name` ASC ORDER BY `age` DESC LIMIT ?,?");
+        Assert.assertArrayEquals(createArrays(params),createArrays(sql.getParamsList()));
     }
 }
